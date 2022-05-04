0920667231d13925360055c8e86d5d27bcf739ce
Kian Zohoury <kianzohoury@berkeley.edu>
2022-05-03 18:14:36 -0700

    new dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,17 @@
+from collections import OrderedDict
+from pathlib import Path
+from typing import List
+
+import librosa
 from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
+
 from .datasets import AudioFolder
 
 
-def create_dataset(dataset_params: dict, subset: str = "train") -> AudioFolder:
+def create_audio_folder(
+    dataset_params: dict, subset: str = "train"
+) -> AudioFolder:
     """Instantiates and returns an AudioFolder given a dataset config."""
     audio_folder = AudioFolder(
         subset=subset,
@@ -15,6 +24,31 @@ def create_dataset(dataset_params: dict, subset: str = "train") -> AudioFolder:
         backend=dataset_params["backend"],
     )
     return audio_folder
+
+
+def create_audio_dataset(
+    dataset_path: str, split: str, targets: List[str]
+) -> List[OrderedDict]:
+    """Loads full audio dataset directly into disk memory."""
+    audio_tracks = []
+    num_tracks = len(list(Path(dataset_path, split).iterdir()))
+    with tqdm(Path(dataset_path, split).iterdir(), total=num_tracks) as tq:
+        entry = OrderedDict()
+        for index, track_folder in enumerate(tq):
+            track_name = track_folder / "mixture.wav"
+            mixture_track, sr = librosa.load(track_name, sr=None)
+            entry["mixture"] = mixture_track
+            for target in targets:
+                target_name = f"{str(track_folder)}/{target}.wav"
+                entry[target], sr = librosa.load(target_name, sr=sr)
+            duration = (
+                int(librosa.get_duration(y=mixture_track, sr=44100)) * sr
+            )
+            entry["duration"] = duration
+            audio_tracks.append(entry)
+            if index == num_tracks:
+                break
+    return audio_tracks
 
 
 def load_dataset(dataset: AudioFolder, loader_params: dict) -> DataLoader:
--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,6 +1,12 @@
+from collections import OrderedDict
 from pathlib import Path
 from typing import Iterator, List, Optional, Tuple
-from torch.utils.data.dataset import IterableDataset
+
+from torch import Tensor
+from torch.utils.data.dataset import IterableDataset, Dataset
+from tqdm import tqdm
+
+from . import create_audio_dataset
 
 import librosa
 import numpy as np
@@ -170,3 +176,61 @@ class AudioFolder(IterableDataset):
         while True:
             mix, target = self._generate_mixture()
             yield mix, target
+
+
+class AudioDataset(Dataset):
+    def __init__(
+        self,
+        dataset_path: str,
+        targets: List[str],
+        chunk_size: int = 1,
+        num_chunks: int = int(1e6),
+        split="train",
+    ):
+        super(AudioDataset, self).__init__()
+        self.dataset_path = dataset_path
+        self.targets = targets
+        self.split = split
+        full_dataset = create_audio_dataset(
+            dataset_path=dataset_path, targets=targets, split=split
+        )
+        self.dataset = self.make_chunks(
+            dataset=full_dataset, chunk_size=chunk_size, num_chunks=num_chunks
+        )
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
+        mixture = torch.from_numpy(self.dataset[idx]['mixture'])
+        targets = []
+        for target in self.targets:
+            targets.append(torch.from_numpy(self.dataset[idx][target]))
+        targets = torch.stack(targets, dim=-1)
+        return mixture, targets
+
+    @staticmethod
+    def make_chunks(dataset, chunk_size: int, num_chunks: int):
+        chunked_dataset = []
+        num_tracks = len(dataset)
+        with tqdm(range(num_chunks), total=num_chunks) as tq:
+            for index in enumerate(tq):
+
+                entry = dataset[np.random.randint(num_tracks)]
+                mixture = entry["mixture"]
+                duration = entry["duration"]
+
+                offset = np.random.randint(0, duration - chunk_size * 44100)
+                stop = offset + int(44100 * chunk_size)
+                mix_chunk = mixture[offset:stop]
+
+                chunked_entry = OrderedDict()
+                chunked_entry["mixture"] = mix_chunk
+                for target_name, target_data in list(entry.items())[1:-1]:
+                    chunked_entry[target_name] = target_data[offset:stop]
+                chunked_dataset.append(chunked_entry)
+
+                if index == num_chunks:
+                    break
+        del dataset
+        return chunked_dataset
