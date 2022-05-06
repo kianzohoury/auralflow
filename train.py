a9ac6c3ead1615ba3374193a55554cbd6e29528b
Kian Zohoury <kianzohoury@berkeley.edu>
2022-05-05 20:12:57 -0700

    dfsd

--- train.py
+++ train.py
@@ -25,27 +25,27 @@ def run_tensorboard(log_dir):
     # os.chdir(Path(__file__).parent / "runs")
     # [f"python -m tensorboard.main --logdir {log_dir}"]
     
-    tb_path = ["tensorboard", "--logdir", 'logs']
+    command = ["tensorboard", "--logdir", 'logs']
     # command = ["python", "-m", "tensorboard.main", "--logdir", log_dir]
     # command = tb_path
     # print(command)
     # tb_main.main()
     # log = logging.getLogger('werkzeug').setLevel(logging.ERROR)
         # Start tensorboard server
-    tb.program.FLAGS.logdir = os.getcwd() + '/logs'
-    tb.program.main(tb.default.get_plugins(),
-                tb.default.get_assets_zip_provider())
+    # tb.program.FLAGS.logdir = os.getcwd() + '/logs'
+    # tb.program.main(tb.default.get_plugins(),
+    #             tb.default.get_assets_zip_provider())
     # tb_ = program.TensorBoard(tb_def.get_plugins())
     # tb.configure(argv=[None, '--logdir', 'logs'])
     # url = tb.launch()
     # sys.stdout.write('TensorBoard at %s \n' % url)
     # print(os.system("tensorboard --logdir runs"))
     
-    # tb_thread = threading.Thread(
-    #     target=lambda: subprocess.Popen(command, shell=False, cwd=os.getcwd()).communicate(),
-    #     daemon=True,
-    # )
-    # tb_thread.start()
+    tb_thread = threading.Thread(
+        target=lambda: subprocess.Popen(command, shell=False, cwd=os.getcwd()).communicate(),
+        daemon=True,
+    )
+    tb_thread.start()
     # print(subprocess.Popen(command, shell=False, cwd=os.getcwd()).communicate())
     # print(subprocess.check_output(command), shell=True)
     sys.exit(1)
@@ -186,9 +186,9 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     run_tensorboard("logs")
-    # writer_process = Process(
-    #     target=run_tensorboard, args=("runs",)
-    # )
+    writer_process = Process(
+        target=run_tensorboard, args=("runs",)
+    )
 
     # writer_process.start()
     # main_process = Process(target=main, args=(args.config_filepath,))
