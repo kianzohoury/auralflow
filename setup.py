99390803fceb643f002445929e7f3d4f27c4bc91
Kian Zohoury <kianzohoury@berkeley.edu>
2022-05-19 01:48:19 -0700

    added metrics

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 Kian Zohoury
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+# This code is part of the auralflow project linked below.
+# https://github.com/kianzohoury/auralflow.git
+
 from setuptools import find_packages, setup
 from pathlib import Path
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 Kian Zohoury
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+# This code is part of the auralflow project linked below.
+# https://github.com/kianzohoury/auralflow.git
+
 import json
 import torch
 
