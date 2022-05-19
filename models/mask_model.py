dedbdbf9c9a3e7d63581fe1ba1908300cacc93da
kian <kzohoury@berkeley.edu>
2022-05-19 15:36:35 +0000

    changes from colab notebook

--- models/mask_model.py
+++ models/mask_model.py
@@ -8,6 +8,7 @@ import copy
 import torch
 import torch.nn as nn
 
+
 from .base import SeparationModel
 from torch import Tensor, FloatTensor
 from typing import Optional
@@ -66,22 +67,7 @@ class SpectrogramMaskModel(SeparationModel):
         )
 
         self.scale = 1e3
-        model_copy = copy.deepcopy(self.model)
-        self.f32_weights = self.copy_params(self.model, model_copy)
-
-    @staticmethod
-    def copy_params(module_src, module_dest):
-        params_dest = dict(module_dest.named_parameters())
-        for name, param in module_src.named_parameters():
-            params_dest[name].data.copy_(param.data)
-        return params_dest
-
-    def update_f32_gradients(self):
-        for name, param in self.model.named_parameters():
-            if param.grad is not None:
-                self.f32_weights[name].grad = param.grad.to(dtype=torch.float32)
-                self.f32_weights[name].grad /= self.scale
-        self.model._parameters = self.f32_weights
+        self.f32_weights = copy.deepcopy(list(self.model.parameters()))
 
     def set_data(self, mix: Tensor, target: Optional[Tensor] = None) -> None:
         """Wrapper method processes and sets data for internal access."""
@@ -118,8 +104,6 @@ class SpectrogramMaskModel(SeparationModel):
         self.criterion()
         # Apply scaling.
         self.batch_loss = self.scale * self.batch_loss
-        print()
-        print(self.batch_loss)
         return self.batch_loss.item()
 
     def backward(self) -> None:
@@ -129,17 +113,16 @@ class SpectrogramMaskModel(SeparationModel):
     def optimizer_step(self) -> None:
         """Updates model's parameters."""
         self.train()
-        skip_update = False
-        for param in self.model.parameters():
-            if param.grad is not None:
-                if param.grad.isnan().any() or param.grad.isinf().any():
-                    skip_update = True
-        if not skip_update:
-            self.update_f32_gradients()
-            grad_norm = nn.utils.clip_grad_norm_(
-                self.model.parameters(), max_norm=2e10
-            )
-            print(grad_norm)
+        for param in self.f32_weights:
+            if param.grad is not None and not param.grad.isnan().any() and not param.grad.isinf().any():
+                param.grad /= self.scale
+            # elif param.grad is not None:
+            #     self.scale /= 2
+                
+        # self.grad_scaler.unscale_(self.optimizer)
+
+        grad_norm = nn.utils.clip_grad_norm_(self.f32_weights, max_norm=2e10)
+        print(grad_norm)
 
         # self.grad_scaler.step(self.optimizer)
         # self.grad_scaler.update()
@@ -149,13 +132,11 @@ class SpectrogramMaskModel(SeparationModel):
         #         grad_norm = torch.linalg.norm(param.grad)
         #         print(f"weight norm: {weight_norm} \n grad norm {grad_norm}")
         self.optimizer.step()
-
         # Quicker gradient zeroing.
         for param in self.model.parameters():
             param.grad = None
 
-        model_copy = copy.deepcopy(self.model)
-        self.f32_weights = self.copy_params(self.model, model_copy)
+        self.f32_weights = copy.deepcopy(list(self.model.parameters()))
 
     def scheduler_step(self) -> bool:
         """Reduces lr if val loss does not improve, and signals early stop."""
@@ -177,7 +158,7 @@ class SpectrogramMaskModel(SeparationModel):
         visualizer.visualize_gradient(model=self, global_step=epoch)
 
     def post_epoch_callback(
-            self, mix: Tensor, target: Tensor, visualizer: Visualizer, epoch: int
+        self, mix: Tensor, target: Tensor, visualizer: Visualizer, epoch: int
     ) -> None:
         """Logs images and audio to tensorboard at the end of each epoch."""
         visualizer.visualize(
