b528f77eca24a1339ac25e6118b9fbc8c305c707
kian <kzohoury@berkeley.edu>
2022-06-01 06:57:56 +0000

    changes from colab notebook

--- auralflow/test.py
+++ auralflow/test.py
@@ -1,8 +1,9 @@
-from csv import writer
+
 from pathlib import Path
 
 import asteroid.metrics
 import librosa
+import csv
 
 from auralflow.models import create_model, setup_model
 from auralflow.separate import separate_audio
@@ -24,13 +25,12 @@ def main(config_filepath: str, save_filepath: str) -> None:
     print("  Successful.")
 
     # Path to test set.
-    test_filepath = model.training_params["path"] + "/test"
+    test_filepath = model.dataset_params["dataset_path"] + "/test"
     global_metrics = {
         "pesq": 0,
         "sar": 0,
         "sdr": 0,
         "si_sdr": 0,
-        "sir": 0,
         "stoi": 0,
     }
     table_entries = []
@@ -38,37 +38,47 @@ def main(config_filepath: str, save_filepath: str) -> None:
     for track_name in Path(test_filepath).iterdir():
         label = model.target_labels[0]
 
-        # Get stems.
-        stems = separate_audio(
-            model=model, filename=str(track_name), sr=44100
-        )
-
         # Load target audio.
         target_audio, sr = librosa.load(
             str(track_name) + f"/{label}.wav", sr=44100
         )
 
+        # Get stems.
+        stems = separate_audio(
+            model=model,
+            filename=str(track_name),
+            sr=44100,
+            duration=4
+        )
+
+        max_frames = stems["estimate"].shape[-1]
+
         named_metrics = asteroid.metrics.get_metrics(
-            mix=stems["mix"],
-            clean=target_audio,
-            estimate=stems["estimate"],
+            mix=stems["mix"][..., :max_frames].cpu().numpy(),
+            clean=target_audio[..., :max_frames],
+            estimate=stems["estimate"].cpu().numpy(),
             sample_rate=sr,
             metrics_list="all",
             ignore_metrics_errors=True,
             average=True,
         )
 
-        row = [track_name]
-        for metric_label in sorted(named_metrics.keys()):
-            global_metrics[metric_label] += named_metrics[metric_label]
-            row.append(named_metrics[metric_label])
+        row = {"track_name": track_name.name}
+        for metric_label, val in named_metrics.items():
+            if metric_label in global_metrics and val is not None:
+                global_metrics[metric_label] += val
+                row[metric_label] = val
         table_entries.append(row)
 
-    row = ["mean"]
-    for metric_label in sorted(global_metrics.keys()):
-        row.append(global_metrics[metric_label] / len(table_entries))
+    row = {"track_name": "average"}
+    for metric_label, val in global_metrics.items():
+        row[metric_label] = global_metrics[metric_label] / len(table_entries)
     table_entries.append(row)
 
+    column_labels = ["track_name"] + sorted(global_metrics.keys())
+
     # Save as csv file.
-    with open(save_filepath + "/metrics.csv", mode="w") as file:
-        writer(csvfile=file)
+    with open(save_filepath + "/metrics.csv", mode="w", newline="") as csvfile:
+        csv_writer = csv.DictWriter(csvfile, fieldnames=column_labels)
+        csv_writer.writeheader()
+        csv_writer.writerows(table_entries)
