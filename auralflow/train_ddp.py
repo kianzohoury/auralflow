1d30cde76461a89ed69f676b77fdea22e7eb69ff
Kian Zohoury <kianzohoury@berkeley.edu>
2022-11-17 02:10:56 -0800

    added config to ddp

--- auralflow/train_ddp.py
+++ auralflow/train_ddp.py
@@ -5,6 +5,8 @@
 # https://github.com/kianzohoury/auralflow.git
 
 import auralflow.__main__ as main_script
+import torch.cuda
+import shutil
 from auralflow import configurations
 from auralflow import parse
 import sys
@@ -26,47 +28,31 @@ def model_fn(model_dir):
     print("  Successful.")
 
 
-
 if __name__ == "__main__":
-    
-    # manually hack arguments for now
-    print(sys.argv)
+    config_params = {
+        key.replace("-", "_"): val for key, val in os.environ["SM_HPS"]
+    }
+    targets = []
+    for key in config_params:
+        if key in {"bass", "drums", "vocals", "other"}:
+            targets.append(key)
 
-        # Create model configuration from args.
-        model_config = configurations._create_model_config(
-            model_type=args.__dict__.pop("model_type"),
-            targets=parse.parse_targets(args),
-            **args.__dict__
-        )
+    # Create model configuration from args.
+    model_config = configurations._create_model_config(
+        model_type=config_params.pop("model_type"),
+        targets=sorted(targets),
+        **config_params
+    )
 
-        # Save the model configuration file within the specified save dir.
-        save_dir = Path(args.save)
-        if save_dir.is_dir():
-            shutil.rmtree(str(save_dir))
-        save_dir.mkdir(parents=True, exist_ok=True)
-        model_config.save(filepath=str(save_dir.joinpath("model.json")))
+    # Save the model configuration file within the specified save dir.
+    save_dir = Path(os.environ["SM_OUTPUT_DATA_DIR"], config_params["save"])
+    if save_dir.is_dir():
+        shutil.rmtree(str(save_dir))
+    save_dir.mkdir(parents=True, exist_ok=True)
+    model_config.save(filepath=str(save_dir.joinpath("model.json")))
 
-        # Display model config as a table.
-        if args.display:
-            print(model_config)
+    # Display model config as a table.
+    if config_params[display]:
+        print(model_config)
 
-        print(f"Model configuration successfully saved to {str(save_dir)}.")
-    
-#     i = 0
-#     while i < len(sys.argv):
-#         if sys.argv[i] == "--train":
-#             break
-#         i += 1 
-        
-#     sys.argv = ['/home/ec2-user/SageMaker/auralflow/auralflow/__main__.py'] + sys.argv[1:i]
-    
-    # config model
-#     os.system(f"python3 {file_path} " + " ".join(sys.argv[1:i]))
-#     sys.argv = [file_path] + sys.argv[:i]
-    
-#     print(sys.argv)
-#     main_script.main()
-    
-#     # train model
-#     sys.argv = [file_path] + sys.argv[i:]
-#     auralflow.__main__.main()
+    print(f"Model configuration successfully saved to {str(save_dir)}.")
