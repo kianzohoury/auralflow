9cc220d7434d13d6ee18f77da206c39e79521ee1
kian <kzohoury@berkeley.edu>
2022-11-17 07:45:18 +0000

    Revert "working on training script"
    This reverts commit 505bddf6a4cb6694387f4884de07989397336242

--- auralflow/train.py
+++ auralflow/train.py
@@ -11,16 +11,6 @@ from torch.utils.data import DataLoader
 from pathlib import Path
 
 
-# def model_fn(model_dir):
-    
-    
-# #     model = configurations._build_model(
-# #         model_config=model_config,
-# #         device=training_config.device
-# #     )
-
-
-
 def main(
     model_config: configurations.AudioModelConfig,
     training_config: configurations.TrainingConfig,
