9712add6f0e5d7ccccd0ea5eee75ac5479d09882
Kian Zohoury <kianzohoury@berkeley.edu>
2022-11-17 03:00:28 -0800

    working on fixing parsing for ddp

--- auralflow/train_ddp.py
+++ auralflow/train_ddp.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     print(os.environ["SM_HPS"])
     
     config_params = json.loads(os.environ["SM_HPS"])
+    print(config_params)
     config_params = {
         key.replace("-", "_"): val for (key, val) in config_params.items()
     }
@@ -56,5 +57,62 @@ if __name__ == "__main__":
         print(model_config)
 
     print(f"Model configuration successfully saved to {str(save_dir)}.")
-
-    
+    #
+    # # Create loss criterion configuration.
+    # if isinstance(model_config, configurations.SpecModelConfig):
+    #     input_type = "spectrogram"
+    # else:
+    #     input_type = "audio"
+    # criterion_config = configurations.CriterionConfig.from_dict(
+    #     input_type=input_type, **args.__dict__
+    # )
+    #
+    # # Set logging and image directories.
+    # if not args.tensorboard:
+    #     logging_dir = image_dir = None
+    # else:
+    #     # Assign to default directories.
+    #     logging_dir = str(save_dir.joinpath("runs"))
+    #     image_dir = str(save_dir)
+    #
+    # # Create visualization configuration.
+    # visuals_config = configurations.VisualsConfig.from_dict(
+    #     logging_dir=logging_dir,
+    #     image_dir=image_dir,
+    #     **args.__dict__
+    # )
+    #
+    # # Create trainer configuration.
+    # training_config = configurations.TrainingConfig.from_dict(
+    #     criterion_config=criterion_config,
+    #     visuals_config=visuals_config,
+    #     checkpoint=str(save_dir.joinpath("checkpoint.pth")),
+    #     device="cuda" if torch.cuda.is_available() else "cpu",
+    #     **args.__dict__
+    # )
+    #
+    # # Save the training configuration in the same directory.
+    # training_config.save(filepath=str(save_dir.joinpath("trainer.json")))
+    #
+    # if not args.resume:
+    #     # Delete existing metadata files.
+    #     for metadata_file in list(save_dir.glob("*.pickle")):
+    #         metadata_file.unlink()
+    #     # Delete existing checkpoint.
+    #     prev_checkpoint = save_dir.joinpath("checkpoint.pth")
+    #     if prev_checkpoint.exists():
+    #         prev_checkpoint.unlink()
+    #
+    # if args.display:
+    #     print(training_config)
+    #
+    # # Run training.
+    # train.main(
+    #     model_config=model_config,
+    #     save_dir=str(save_dir),
+    #     training_config=training_config,
+    #     dataset_path=args.dataset_path,
+    #     max_num_tracks=args.max_tracks,
+    #     max_num_samples=args.max_samples,
+    #     resume=args.resume
+    # )
